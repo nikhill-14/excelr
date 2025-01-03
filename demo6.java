61d456313f2c235946e9678d985823e39bee980f
nikhill-14 <nikhilgoud9346@gmail.com>
2025-01-03 10:12:08 +0530

    Add files via upload

---
+++ demo6.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String username = "Nikhil";
+        String password = "Nikhil@123";
+         while ((username == "Nikhil") && (password == "Nikhil@123")) {
+          System.out.println("hello! Nikhil");
+        break;
+    }
+}
+}
